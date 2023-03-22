98f6a48c77859dc657943ba68ca0067ab37e44d5
Nick Wright <nwright@egnyte.com>
2023-03-22 13:57:11 -0400

    react a mole

---
+++ src/Components/Mole.js
@@ -0,0 +1,22 @@
+import {useEffect} from 'react'
+import MoleImg from './mole.png'
+
+
+
+function Mole (props){
+
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 10000)
+        let timer = setTimeout(() => {
+            props.toggle(false)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+    return (
+        <div>
+            <img style={{'width': '30vw'}} src={MoleImg} onClick={props.handleClick} />
+        </div>
+    )
+}
+
+export default Mole;
